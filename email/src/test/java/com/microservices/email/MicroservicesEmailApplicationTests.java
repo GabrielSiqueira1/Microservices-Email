807b2ba9e1531472388fcecfff0132149096509b
Gabriel Siqueira <109818266+GabrielSiqueira1@users.noreply.github.com>
2022-10-20 10:20:35 -0300

    Add files via upload

---
+++ email/src/test/java/com/microservices/email/MicroservicesEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
